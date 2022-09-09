5a45226bbbe3bbd6c11288949ee13f454232c918
PauloMtz <pramosmtz@gmail.com>
2022-09-09 07:13:23 -0300

    Rota para usuários não logados

---
+++ src/routes/auth.routes.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+// expo install @react-navigation/native
+// expo install react-native-screens react-native-safe-area-context
+// expo install @react-navigation/native-stack
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import SignIn from "../pages/SignIn";
+
+const Stack = createNativeStackNavigator();
+
+// rotas para usuários não logados
+function AuthRoutes() {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen name="SignIn" component={ SignIn } />
+        </Stack.Navigator>
+    )
+}
+
+export default AuthRoutes;

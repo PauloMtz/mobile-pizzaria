766c1cfd8395ff0bbd404a0265d48b15027cb58c
PauloMtz <pramosmtz@gmail.com>
2022-09-13 07:18:58 -0300

    Tela Novo Pedido

--- src/pages/Order/index.tsx
+++ src/pages/Order/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity, TextInput } from "react-native";
 import { useRoute, RouteProp } from "@react-navigation/native";
+import { Feather } from '@expo/vector-icons';
 
 type RouteDetailParams = {
     Order: {
@@ -16,14 +17,107 @@ export default function Order() {
 
     return (
         <View style={styles.container}>
-            <Text>Pedido</Text>
-            <Text>Mesa: {route.params.number}</Text>
+            <View style={styles.header}>
+                <Text style={styles.title}>Mesa: {route.params.number}</Text>
+
+                <TouchableOpacity>
+                    <Feather name="trash-2" size={28} color="#FF3F4B" />
+                </TouchableOpacity>
+            </View>
+
+            <TouchableOpacity style={styles.input}>
+                <Text style={{color: '#FFF'}}>Pizzas</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.input}>
+                <Text style={{color: '#FFF'}}>Pizza Calabresa</Text>
+            </TouchableOpacity>
+
+            <View style={styles.qtdContainer}>
+                <Text style={styles.qtdTexto}>Quantidade</Text>
+                <TextInput placeholder="1" placeholderTextColor="#F0F0F0"
+                    keyboardType="numeric" value="1" 
+                    style={[styles.input, {width:'60%', textAlign:'center'}]} />
+            </View>
+
+            <View style={styles.actions}>
+                <TouchableOpacity style={styles.buttonAdd}>
+                    <Text style={styles.buttonText}>+</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={styles.button}>
+                    <Text style={styles.buttonText}>Avançar</Text>
+                </TouchableOpacity>
+            </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-        //
+        flex: 1, // pega toda a tela
+        backgroundColor: '#1d1d2e',
+        paddingVertical: '5%',
+        paddingEnd: '4%',
+        paddingStart: '4%'
+    },
+    header: {
+        flexDirection: 'row',
+        marginBottom: 12,
+        alignItems: 'center',
+        marginTop: 24
+    },
+    title: {
+        fontSize: 30,
+        color: '#FFF',
+        fontWeight: 'bold',
+        marginRight: 14
+    },
+    input: {
+        backgroundColor: '#101026',
+        borderRadius: 4,
+        width: '100%',
+        height: 46,
+        marginBottom: 12,
+        justifyContent: 'center',
+        paddingHorizontal: 8,
+        color: '#FFF',
+        fontSize: 20
+    },
+    qtdContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between'
+    },
+    qtdTexto: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: '#FFF'
+    },
+    actions: {
+        flexDirection: 'row',
+        width: '100%',
+        justifyContent: 'space-between'
+    },
+    buttonAdd: {
+        width: '20%',
+        backgroundColor: '#3fd1ff',
+        borderRadius: 4,
+        height: 40,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    buttonText: {
+        color: '#101026',
+        fontSize: 18,
+        fontWeight: 'bold'
+    },
+    button: {
+        backgroundColor: '#3fffa3',
+        borderRadius: 4,
+        height: 40,
+        width: '75%',
+        alignItems: 'center',
+        justifyContent: 'center'
     }
 });
